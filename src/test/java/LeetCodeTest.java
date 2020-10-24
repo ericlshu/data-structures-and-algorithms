b315225909f800500bed53fffba9843e93d19a94
shuli <outlieric@vip.qq.com>
2020-10-24 22:11:32 +0800

    2020-10-24: Video Stitching;

--- src/test/java/LeetCodeTest.java
+++ src/test/java/LeetCodeTest.java
@@ -60,10 +60,10 @@ public class LeetCodeTest
     {
         P0019_RemoveNthNodeFromEndOfList obj = new P0019_RemoveNthNodeFromEndOfList();
         ListNode node5 = new ListNode(5);
-        ListNode node4 = new ListNode(4,node5);
-        ListNode node3 = new ListNode(3,node4);
-        ListNode node2 = new ListNode(2,node3);
-        ListNode node1 = new ListNode(1,node2);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
         ListNode node = obj.removeNthFromEnd(node1, 3);
         System.out.println("node = " + node);
     }
@@ -81,11 +81,12 @@ public class LeetCodeTest
         P0844_BackspaceStringCompare obj = new P0844_BackspaceStringCompare();
         System.out.println("result = " + obj.backspaceString("oi###mu#ueo##pk#o##rsztu#y#s#xu###bxx##dqc#gz#djz"));
     }
+
     @Test
     public void testP0014()
     {
         P0014_LongestCommonPrefix obj = new P0014_LongestCommonPrefix();
-        System.out.println("result = " + obj.longestCommonPrefix(new String[] {"ab","a"}));
+        System.out.println("result = " + obj.longestCommonPrefix(new String[]{"ab", "a"}));
     }
 
     @Test
@@ -93,10 +94,10 @@ public class LeetCodeTest
     {
         P0143_ReorderList obj = new P0143_ReorderList();
         ListNode node5 = new ListNode(5);
-        ListNode node4 = new ListNode(4,node5);
-        ListNode node3 = new ListNode(3,node4);
-        ListNode node2 = new ListNode(2,node3);
-        ListNode node1 = new ListNode(1,node2);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
         obj.reorderList(node1);
     }
 
@@ -104,13 +105,21 @@ public class LeetCodeTest
     public void testP0925()
     {
         P0925_LongPressedName obj = new P0925_LongPressedName();
-        System.out.println("result = " + obj.isLongPressedName("saeedi","ssaaeediixxxiii"));
+        System.out.println("result = " + obj.isLongPressedName("saeedi", "ssaaeediixxxiii"));
     }
 
     @Test
     public void testP0028()
     {
         P0028_ImplementStrStr obj = new P0028_ImplementStrStr();
-        System.out.println("result = " + obj.strStr("mississippi","issippip"));
+        System.out.println("result = " + obj.strStr("mississippi", "issippip"));
+    }
+
+    @Test
+    public void testP1024()
+    {
+        P1024_VideoStitching obj = new P1024_VideoStitching();
+        int[][] clips = {{0, 1}, {6, 8}, {0, 2}, {5, 6}, {0, 4}, {0, 3}, {6, 7}, {1, 3}, {4, 7}, {1, 4}, {2, 5}, {2, 6}, {3, 4}, {4, 5}, {5, 7}, {6, 9}};
+        System.out.println("result = " + obj.videoStitching(clips, 9));
     }
 }
