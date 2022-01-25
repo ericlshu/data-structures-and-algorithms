5aaf88fbb5e5d9b44fbd5bd2efc011f702c644ba
Eric SHU <shuli0502@gmail.com>
2022-01-25 14:52:14 +0800

    Binary Tree Preorder Traversal;

--- src/main/java/domain/TreeNode.java
+++ src/main/java/domain/TreeNode.java
@@ -28,4 +28,14 @@ public class TreeNode
         this.left = left;
         this.right = right;
     }
+
+    @Override
+    public String toString()
+    {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
