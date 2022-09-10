c329d54bc0945c53854f051c05fb54cf533f393c
Eric SHU <shuli0502@gmail.com>
2022-09-10 13:30:28 +0800

    242. 有效的字母异位词-Unicode扩展；

--- src/main/java/solution/array/P0242ValidAnagram.java
+++ src/main/java/solution/array/P0242ValidAnagram.java
@@ -1,6 +1,8 @@
 package solution.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Description : 242. 有效的字母异位词
@@ -78,6 +80,24 @@ public class P0242ValidAnagram
         for (int i = 0; i < 26; i++)
             if (alpha[i] != 0)
                 return false;
+        return true;
+    }
+
+    public boolean isAnagram_hashmap(String s, String t)
+    {
+        if (s.length() != t.length())
+            return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++)
+        {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+            if (map.get(t.charAt(i)) < 0)
+                return false;
+        }
         return true;
     }
 }
