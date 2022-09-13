79d298988dbbe053eb4b51ed9be0904b47dc4a95
Eric SHU <shuli0502@gmail.com>
2022-09-13 21:17:08 +0800

    670. 最大交换-贪心算法；

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,12 +1,7 @@
 import org.junit.Test;
 import solution.dynamic_programming.Offer101FibonacciSequence;
-import solution.math.OfferII072;
-import solution.math.P0007ReverseInteger;
-import solution.math.P0009PalindromeNumber;
-import solution.math.P0509FibonacciNumber;
-import solution.math.P1137NthTribonacciNumber;
-
-import java.util.Arrays;
+import solution.greedy.P0670MaximumSwap;
+import solution.math.*;
 
 /**
  * Description :
@@ -42,25 +37,35 @@ public class MathTest
     }
 
     @Test
-    public void testOffer101(){
+    public void testOffer101()
+    {
         Offer101FibonacciSequence obj = new Offer101FibonacciSequence();
         System.out.println("result = " + obj.fib3(30));
-        System.out.println("mul = " + obj.fib4(30) );
+        System.out.println("mul = " + obj.fib4(30));
     }
 
     @Test
-    public void testP0509(){
+    public void testP0509()
+    {
         P0509FibonacciNumber obj = new P0509FibonacciNumber();
         System.out.println("result = " + obj.fib(37));
         System.out.println("result = " + obj.fibonacci(37));
     }
 
     @Test
-    public void test1137(){
+    public void test1137()
+    {
         P1137NthTribonacciNumber obj = new P1137NthTribonacciNumber();
         System.out.println("result1 = " + obj.tribonacci_1(37));
         System.out.println("result2 = " + obj.tribonacci_2(37));
         System.out.println("result3 = " + obj.tribonacci_3(37));
     }
 
+    @Test
+    public void test0670()
+    {
+        P0670MaximumSwap obj = new P0670MaximumSwap();
+        System.out.println(obj.maximumSwap_1(2736));
+        System.out.println(obj.maximumSwap_2(9973));
+    }
 }
