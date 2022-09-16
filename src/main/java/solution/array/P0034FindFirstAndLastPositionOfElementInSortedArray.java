e854a43bc4453e45088d5ecd5958b3068c0463d6
Eric SHU <shuli0502@gmail.com>
2022-09-16 20:57:10 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置;

--- src/main/java/solution/array/P0034FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/solution/array/P0034FindFirstAndLastPositionOfElementInSortedArray.java
@@ -31,15 +31,19 @@ package solution.array;
  * @date 2021-04-25 6:09 PM
  * @since JDK 1.8
  */
-public class P0034FindFirstAndLastPositionOfElementInSortedArray {
-    public int[] searchRange(int[] nums, int target) {
+public class P0034FindFirstAndLastPositionOfElementInSortedArray
+{
+    public int[] searchRange(int[] nums, int target)
+    {
         int[] result = new int[]{-1, -1};
         if (nums == null || nums.length == 0 || nums[0] > target || nums[nums.length - 1] < target)
             return result;
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
+        while (left <= right)
+        {
             int mid = (left + right) / 2;
-            if (nums[mid] == target) {
+            if (nums[mid] == target)
+            {
                 left = right = mid;
                 while (left >= 0 && nums[left] == target)
                     result[0] = left--;
@@ -47,13 +51,41 @@ public class P0034FindFirstAndLastPositionOfElementInSortedArray {
                     result[1] = right++;
                 return result;
             }
-            else if (nums[mid] > target) {
+            else if (nums[mid] > target)
                 right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return result;
+    }
+
+    public int[] searchRange_1(int[] nums, int target)
+    {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target)
+        {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower)
+    {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target))
+            {
+                right = mid - 1;
+                ans = mid;
             }
-            else {
+            else
+            {
                 left = mid + 1;
             }
         }
-        return result;
+        return ans;
     }
 }
