5f13299d1b0320bb7c0c833c5f0047dad13926d5
Eric SHU <shuli0502@gmail.com>
2022-09-18 20:13:17 +0800

    167. 两数之和 II - 输入有序数组；

--- src/main/java/solution/array/P0167TwoSum_II_InputArrayIsSorted.java
+++ src/main/java/solution/array/P0167TwoSum_II_InputArrayIsSorted.java
@@ -52,7 +52,7 @@ public class P0167TwoSum_II_InputArrayIsSorted
         return new int[]{-1, -1};
     }
 
-    public int[] twoSum(int[] numbers, int target)
+    public int[] twoSum_2(int[] numbers, int target)
     {
         for (int i = 0; i < numbers.length; i++)
         {
@@ -67,6 +67,25 @@ public class P0167TwoSum_II_InputArrayIsSorted
                     left = mid + 1;
                 else
                     right = mid - 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] twoSum(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.length; ++i)
+        {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high)
+            {
+                int mid = (high + low) >> 1;
+                if (numbers[mid] == target - numbers[i])
+                    return new int[]{i + 1, mid + 1};
+                else if (numbers[mid] > target - numbers[i])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
             }
         }
         return new int[]{-1, -1};
