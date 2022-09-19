ba00625155dca8dcdf07922182b1e8f3ba6150ce
Eric SHU <shuli0502@gmail.com>
2022-09-19 21:56:34 +0800

    74. 搜索二维矩阵；

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -673,4 +673,13 @@ public class ArrayTest
         System.out.println(obj.countNegatives(grid1));
         System.out.println(obj.countNegatives(grid2));
     }
+
+    @Test
+    public void test0074()
+    {
+        P0074SearchA2DMatrix obj = new P0074SearchA2DMatrix();
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        System.out.println(obj.searchMatrix(matrix, 3));
+        System.out.println(obj.searchMatrix(matrix, 13));
+    }
 }
