d3d13327a708bc90b21b6f5a2fefdf15751b112b
Eric SHU <shuli0502@gmail.com>
2022-10-11 21:46:13 +0800

    1281. 整数的各位积和之差;

---
+++ src/main/java/solution/math/P1281SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,41 @@
+package solution.math;
+
+/**
+ * Description : 整数的各位积和之差
+ * 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+ * <p>
+ * 示例 1：
+ * 输入：n = 234
+ * 输出：15
+ * 解释：
+ * 各位数之积 = 2 * 3 * 4 = 24
+ * 各位数之和 = 2 + 3 + 4 = 9
+ * 结果 = 24 - 9 = 15
+ * <p>
+ * 示例 2：
+ * 输入：n = 4421
+ * 输出：21
+ * 解释：
+ * 各位数之积 = 4 * 4 * 2 * 1 = 32
+ * 各位数之和 = 4 + 4 + 2 + 1 = 11
+ * 结果 = 32 - 11 = 21
+ *
+ * @author Eric L SHU
+ * @date 2022-10-11 21:45
+ * @since jdk-11.0.14
+ */
+public class P1281SubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    public int subtractProductAndSum(int n)
+    {
+        int sum = 0, prd = 1;
+        while (n > 0)
+        {
+            int mod = n % 10;
+            sum += mod;
+            prd *= mod;
+            n /= 10;
+        }
+        return prd - sum;
+    }
+}
