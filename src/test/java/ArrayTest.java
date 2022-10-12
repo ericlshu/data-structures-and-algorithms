79b670b2ddcb8d41afa6a9290ff9038b590a3c68
Eric SHU <shuli0502@gmail.com>
2022-10-12 22:25:07 +0800

    48. 旋转图像-矩阵；

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1018,4 +1018,31 @@ public class ArrayTest
         System.out.println(p1779.nearestValidPoint(3, 4, points2));
         System.out.println(p1779.nearestValidPoint(3, 4, points3));
     }
+
+    @Test
+    public void test0048()
+    {
+        P0048RotateImage p0048 = new P0048RotateImage();
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[][] matrix2 = {
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}};
+        p0048.rotate_1(matrix1);
+        p0048.rotate_2(matrix2);
+        System.out.println("matrix1 = " + Arrays.deepToString(matrix1));
+        System.out.println("matrix2 = " + Arrays.deepToString(matrix2));
+
+        matrix2 = new int[][]{
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}};
+        p0048.rotate_3(matrix2);
+        System.out.println("matrix2 = " + Arrays.deepToString(matrix2));
+    }
 }
