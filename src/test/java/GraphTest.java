1017611a13698d4c75e328bd2c61bffe6fb6899d
Eric SHU <shuli0502@gmail.com>
2023-01-08 22:59:22 +0800

    797. 所有可能的路径-DFS&BFS;

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import solution.graph.P0797AllPathsFromSourceToTarget;
 import solution.graph.P1971FindIfPathExistsInGraph;
 
 /**
@@ -20,5 +21,15 @@ public class GraphTest
         System.out.println(p1971.validPath_dfs(3, edges1, 0, 2));
         System.out.println(p1971.validPath_dfs(6, edges2, 0, 5));
         System.out.println(p1971.validPath_bfs(6, edges2, 0, 5));
+    }
+
+    @Test
+    public void test0797()
+    {
+        P0797AllPathsFromSourceToTarget p0797 = new P0797AllPathsFromSourceToTarget();
+        int[][] graph1 = {{1, 2}, {3}, {3}, {}};
+        int[][] graph2 = {{4, 3, 1}, {3, 2, 4}, {3}, {4}, {}};
+        System.out.println(p0797.allPathsSourceTarget_dfs(graph1));
+        System.out.println(p0797.allPathsSourceTarget_bfs(graph2));
     }
 }
