b798520e2970e71396012158d7fd118dde35fa26
Eric SHU <shuli0502@gmail.com>
2023-01-18 22:59:14 +0800

    1797. 设计一个验证系统-哈希表；

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -418,4 +418,26 @@ public class CollectionTest
         System.out.println(p1845.reserve());
         p1845.unreserve(5);
     }
+
+    @Test
+    public void test1917()
+    {
+        P1917AuthenticationManager1 authenticationManager1 = new P1917AuthenticationManager1(5); // 构造 AuthenticationManager ，设置 timeToLive = 5 秒。
+        authenticationManager1.renew("aaa", 1);      // 时刻 1 时，没有验证码的 tokenId 为 "aaa" ，没有验证码被更新。
+        authenticationManager1.generate("aaa", 2);   // 时刻 2 时，生成一个 tokenId 为 "aaa" 的新验证码。
+        System.out.println(authenticationManager1.countUnexpiredTokens(6));      // 时刻 6 时，只有 tokenId 为 "aaa" 的验证码未过期，所以返回 1 。
+        authenticationManager1.generate("bbb", 7);   // 时刻 7 时，生成一个 tokenId 为 "bbb" 的新验证码。
+        authenticationManager1.renew("aaa", 8);      // tokenId 为 "aaa" 的验证码在时刻 7 过期，且 8 >= 7 ，所以时刻 8 的renew 操作被忽略，没有验证码被更新。
+        authenticationManager1.renew("bbb", 10);     // tokenId 为 "bbb" 的验证码在时刻 10 没有过期，所以 renew 操作会执行，该 token 将在时刻 15 过期。
+        System.out.println(authenticationManager1.countUnexpiredTokens(15));     // tokenId 为 "bbb" 的验证码在时刻 15 过期，tokenId 为 "aaa" 的验证码在时刻 7 过期，所有验证码均已过期，所以返回 0 。
+
+        P1917AuthenticationManager2 authenticationManager2 = new P1917AuthenticationManager2(5); // 构造 AuthenticationManager ，设置 timeToLive = 5 秒。
+        authenticationManager2.renew("aaa", 1);      // 时刻 1 时，没有验证码的 tokenId 为 "aaa" ，没有验证码被更新。
+        authenticationManager2.generate("aaa", 2);   // 时刻 2 时，生成一个 tokenId 为 "aaa" 的新验证码。
+        System.out.println(authenticationManager2.countUnexpiredTokens(6));      // 时刻 6 时，只有 tokenId 为 "aaa" 的验证码未过期，所以返回 1 。
+        authenticationManager2.generate("bbb", 7);   // 时刻 7 时，生成一个 tokenId 为 "bbb" 的新验证码。
+        authenticationManager2.renew("aaa", 8);      // tokenId 为 "aaa" 的验证码在时刻 7 过期，且 8 >= 7 ，所以时刻 8 的renew 操作被忽略，没有验证码被更新。
+        authenticationManager2.renew("bbb", 10);     // tokenId 为 "bbb" 的验证码在时刻 10 没有过期，所以 renew 操作会执行，该 token 将在时刻 15 过期。
+        System.out.println(authenticationManager2.countUnexpiredTokens(15));     // tokenId 为 "bbb" 的验证码在时刻 15 过期，tokenId 为 "aaa" 的验证码在时刻 7 过期，所有验证码均已过期，所以返回 0 。
+    }
 }
